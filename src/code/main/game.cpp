c3a9914cd1f5e791bc7d47184ad983b7071e6c36
Nick "Synth <114616754+synthownsyou@users.noreply.github.com>
2026-09-14 03:06:36 +0000

    Update game.cpp

--- src/code/main/game.cpp
+++ src/code/main/game.cpp
@@ -65,6 +65,7 @@
 #include <input/touch/touchassetmanager.h>
 #include <input/touch/touchhudrenderer.h>
 #include <input/touch/touchcontrolsconfigurationmanager.h>
+#include <iosplatform.h>
 #endif
 
 //******************************************************************************
